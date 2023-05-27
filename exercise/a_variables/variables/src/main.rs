a076c94e40f92f884d078800940397801d4b9d99
vitran96 <tranuytrieuvi@gmail.com>
2023-05-27 10:17:54 +0700

    a_variables - part extra

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -2,12 +2,13 @@ const STARTING_MISSILES: i32 = 8;
 const READY_AMOUNT: i32 = 2;
 
 fn main() {
-    let mut missiles = STARTING_MISSILES;
-    let ready = READY_AMOUNT;
+    let (missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+    // let temp = 1;
+    // READY_AMOUNT = 2;
 
     println!("Firing {} of my {} missiles...", ready, missiles);
 
-    missiles -= ready;
+    // missiles -= ready;
 
-    println!("{} missiles left", missiles);
+    println!("{} missiles left", missiles - ready);
 }
