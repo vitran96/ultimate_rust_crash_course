6cf8d742dc399bbece08d6bbf8210e09f4f440aa
vitran96 <tranuytrieuvi@gmail.com>
2023-08-27 09:33:57 +0700

    feature(image_processing) - implemented chaining filter

--- exercise/image_processing/src/main.rs
+++ exercise/image_processing/src/main.rs
@@ -7,184 +7,135 @@ fn main() {
     //
     // Challenge: If you're feeling really ambitious, you could delete this code
     // and use the "clap" library instead: https://docs.rs/clap/2.32.0/clap/
-    let mut args: Vec<String> = std::env::args()
-        .skip(1)
-        .collect();
+    let mut args: Vec<String> = std::env::args().skip(1).collect();
     if args.len() < 3 {
+        println!("Missing arguments.");
         print_usage_and_exit();
     }
-    
+
     //
     let infile = args.remove(0);
     let outfile = args.remove(0);
-    
-    let mut img: DynamicImage = image::open(infile)
-        .expect("Failed to open INFILE.");
 
-    let subcommand = args.remove(0);
+    let mut img: DynamicImage = image::open(infile).expect("Failed to open INFILE.");
 
-    match subcommand.as_str() {
-        // EXAMPLE FOR CONVERSION OPERATIONS
-        "blur" => {
-            if args.len() < 2 {
-                print_usage_and_exit();
+    // Loop until args is empty
+    while args.len() != 0 {
+        let subcommand: String = args.remove(0);
+        match subcommand.as_str() {
+            // EXAMPLE FOR CONVERSION OPERATIONS
+            "blur" => {
+                if args.len() < 1 {
+                    println!("Missing blur_amount");
+                    print_usage_and_exit();
+                }
+
+                let blur_amount: f32 = args
+                    .remove(0)
+                    .parse()
+                    .expect("Failed to parse blur_amount for BLUR");
+
+                img = img.blur(blur_amount);
             }
+
             // **OPTION**
-            // Improve the blur implementation -- see the blur() function below
-            let blur_amount: f32 = args.remove(0)
-                .parse()
-                .unwrap_or(2.0);
-            blur(infile, outfile, blur_amount);
-        }
+            // Brighten -- see the brighten() function below
+            "brighten" => {
+                if args.len() < 1 {
+                    println!("Missing brighten_amount");
+                    print_usage_and_exit();
+                }
 
-        // **OPTION**
-        // Brighten -- see the brighten() function below
-        "brighten" => {
-            if args.len() < 2 {
-                print_usage_and_exit();
-            }
-            
-            let infile = args.remove(0);
-            let outfile = args.remove(0);
-            let brighten_amount: i32 = args.remove(0)
+                let brighten_amount: i32 = args
+                    .remove(0)
                     .parse()
-                    .unwrap_or(10);
-            brighten(infile, outfile, brighten_amount);
-        }
+                    .expect("Failed to parse brighten_amount for BRIGHTEN");
 
-        // **OPTION**
-        // Crop -- see the crop() function below
-        "crop" => {
-            if args.len() < 6 {
-                print_usage_and_exit();
+                img = img.brighten(brighten_amount);
             }
-            
-            let infile = args.remove(0);
-            let outfile = args.remove(0);
-            let x: u32 = args.remove(0)
-                    .parse()
-                    .expect("Failed to parse x for CROP");
-            let y: u32 = args.remove(0)
-                    .parse()
-                    .expect("Failed to parse y for CROP");
-            let width: u32 = args.remove(0)
+
+            // **OPTION**
+            // Crop -- see the crop() function below
+            "crop" => {
+                if args.len() < 4 {
+                    println!("Missing crop arguments");
+                    print_usage_and_exit();
+                }
+
+                let x: u32 = args.remove(0).parse().expect("Failed to parse x for CROP");
+                let y: u32 = args.remove(0).parse().expect("Failed to parse y for CROP");
+                let width: u32 = args
+                    .remove(0)
                     .parse()
                     .expect("Failed to parse width for CROP");
-            let height: u32 = args.remove(0)
+                let height: u32 = args
+                    .remove(0)
                     .parse()
                     .expect("Failed to parse height for CROP");
 
-            crop(infile, outfile, x, y, width, height);
-        }
-
-        // **OPTION**
-        // Rotate -- see the rotate() function below
-        "rotate" => {
-            if args.len() < 2 {
-                print_usage_and_exit();
+                img = img.crop(x, y, width, height)
             }
-            
-            let infile = args.remove(0);
-            let outfile = args.remove(0);
-            let rotation_amount: u32 = args.remove(0)
+
+            // **OPTION**
+            // Rotate -- see the rotate() function below
+            "rotate" => {
+                if args.len() < 1 {
+                    println!("Missing rotation_amount");
+                    print_usage_and_exit();
+                }
+
+                let rotation_amount: u32 = args.remove(0)
                     .parse()
-                    .unwrap_or(90);
-            rotate(infile, outfile, rotation_amount)
-                .expect("Failed to rotate image");
-        }
+                    .expect("Failed to parse rotation_amount for ROTATE");
+                if rotation_amount == 90 {
+                    img = img.rotate90();
+                } else if rotation_amount == 180 {
+                    img = img.rotate180();
+                } else if rotation_amount == 270 {
+                    img = img.rotate270();
+                } else {
+                    println!("Invalid rotation_amount. Must be 90, 180, or 270.");
+                    print_usage_and_exit();
+                }
+            }
 
-        // **OPTION**
-        // Invert -- see the invert() function below
-        "invert" => {
-            if args.len() < 2 {
+            // **OPTION**
+            // Invert -- see the invert() function below
+            "invert" => {
+                img.invert();
+            }
+
+            "grayscale" => {
+                img = img.grayscale();
+            }
+
+            // For everything else...
+            _ => {
+                println!("Unknown subcommand: {}", subcommand);
                 print_usage_and_exit();
             }
-            
-            let infile = args.remove(0);
-            let outfile = args.remove(0);
-            invert(infile, outfile);
-        }
-
-        "grayscale" => {
-            if args.len() < 2 {
-                print_usage_and_exit();
-            }
-            
-            let infile = args.remove(0);
-            let outfile = args.remove(0);
-            grayscale(infile, outfile);
-        }
-
-        // For everything else...
-        _ => {
-            print_usage_and_exit();
         }
     }
+
+    // Save the image to OUTFILE
+    img.save(outfile)
+        .expect("Failed saving OUTFILE");
 }
 
 fn print_usage_and_exit() {
     println!("USAGE (when in doubt, use a .png extension on your filenames)");
-    // println!("blur INFILE OUTFILE");
-    // println!("brighten INFILE OUTFILE");
-    // println!("crop INFILE OUTFILE X Y WIDTH HEIGHT");
-    // println!("rotate INFILE OUTFILE DEGREES{{90, 180, 270}}");
+    println!();
+    println!("INFILE OUTFILE <CHAIN OF FILTERS>");
+    println!();
+    println!("Available filters:");
+    println!("    blur BLUR_AMOUNT");
+    println!("    brighten BRIGHTEN_AMOUNT");
+    println!("    crop X Y WIDTH HEIGHT");
+    println!("    rotate DEGREES{{90, 180, 270}}");
+    println!("    invert");
+    println!("    grayscale");
 
-    // println!("grayscale INFILE OUTFILE");
     std::process::exit(-1);
-}
-
-fn blur(img: DynamicImage, blur_amount: f32) -> DynamicImage {
-    // Here's how you open an existing image file
-    img.blur(blur_amount)
-}
-
-fn brighten(img: DynamicImage, brighten_amount: i32) -> DynamicImage {
-    img.brighten(brighten_amount)
-}
-
-fn crop(
-    mut img: DynamicImage
-    , outfile: String
-    , x: u32
-    , y: u32
-    , width: u32
-    , height: u32
-) -> DynamicImage {
-    img.crop(x, y, width, height)
-}
-
-fn rotate(infile: DynamicImage, rotation_amount: u32) {
-    // Challenge: parse the rotation amount from the command-line, pass it
-    // through to this function to select which method to call.
-    // let img2: DynamicImage;
-    // if rotation_amount == 90 {
-    //     img2 = img.rotate90();
-    // } else if rotation_amount == 180 {
-    //     img2 = img.rotate180();
-    // } else if rotation_amount == 270 {
-    //     img2 = img.rotate270();
-    // } else {
-    //     panic!("Invalid rotation amount")
-    // }
-
-    // Ok(String::from("Ok"))
-}
-
-fn invert(infile: String, outfile: String) {
-    // See blur() for an example of how to open an image.
-    let mut img: DynamicImage = image::open(infile).expect("Failed to open INFILE.");
-    
-    // .invert() takes no arguments and converts the image in-place, so you
-    // will use the same image to save out to a different file.
-    img.invert();
-    
-    // See blur() for an example of how to save the image.
-    img.save(outfile)
-        .expect("Failed writing OUTFILE.");
-}
-
-fn grayscale(img: DynamicImage) -> DynamicImage {
-    img.grayscale()
 }
 
 // **SUPER CHALLENGE FOR LATER** - Let's face it, you don't have time for this during class.
